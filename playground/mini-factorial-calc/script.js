3ba33ee7887023863d9069edf4c335a1c1570330
shourya_malviya23 <shouryamalviya3@gmail.com>
2026-05-03 00:21:06 +0530

    built a mini factorial calculator (non recursive)

---
+++ playground/mini-factorial-calc/script.js
@@ -0,0 +1,14 @@
+let num = 14;
+function factorialCalculator(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+let factorial = factorialCalculator(num);
+
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
